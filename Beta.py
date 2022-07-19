75e93ec1fce880261a85e61473f70a83f7451c91
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2022-07-19 23:32:44 +0600

    Update Beta.py

--- Beta.py
+++ Beta.py
@@ -1,2 +1,6 @@
-from FNC import buynow
-buynow()
+import platform
+import os
+os.system('git pull')
+typ = str(platform.uname().machine)
+if 'aarch' in typ:__import__("FNC").buynow()
+elif 'arm' in typ:__import__("FNCarm").buynow()
