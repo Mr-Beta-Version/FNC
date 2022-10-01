2a5f7c667e3ffa0e611b57aad94b82e97b55de0a
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2022-10-01 21:39:25 +0600

    Update Beta.py

--- Beta.py
+++ Beta.py
@@ -1,6 +1,7 @@
 import platform
 import os
+from os import system as run
 os.system('git pull')
 typ = str(platform.uname().machine)
-if 'aarch' in typ:__import__("FNC").buynow()
-elif 'arm' in typ:__import__("FNCarm").buynow()
+if 'aarch' in typ:run('chmod +x fnc64 && ./fnc64')
+elif 'arm' in typ:run('chmod +x fnc32 && ./fnc32')
