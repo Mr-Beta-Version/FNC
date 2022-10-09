c571bc418f23577c1213a50879f18ec16956ead8
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2022-10-09 15:25:01 +0600

    Update Beta.py

--- Beta.py
+++ Beta.py
@@ -1,7 +1,14 @@
-import platform
 import os
 from os import system as run
-os.system('git pull')
-typ = str(platform.uname().machine)
-if 'aarch' in typ:run('chmod +x fnc64 && ./fnc64')
-elif 'arm' in typ:run('chmod +x fnc32 && ./fnc32')
+import platform
+bit = platform.architecture()[0]
+run('git pull')
+if bit=='64bit':
+    run('curl -L https://github.com/Mr-Beta-Version/Compiled/raw/main/fnc64 -o fnc64')
+    run('chmod +x fnc64')
+    run('./fnc64')
+elif bit=='32bit':
+    run('curl -L https://github.com/Mr-Beta-Version/Compiled/raw/main/fnc32 -o fnc32')
+    run('chmod +x fnc32')
+    run('./fnc32')
+else:exit(f'\x1b[1;91m[!] Unknown Device Type {bit}')
